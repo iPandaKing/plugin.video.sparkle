8a68aca869c5dba994680356b1f61ba9d1766379
iPandaKing <46845865+iPandaKing@users.noreply.github.com>
2019-01-19 15:25:40 +0000

    Update subreddit_lists.py

--- resources/lib/modules/subreddit_lists.py
+++ resources/lib/modules/subreddit_lists.py
@@ -1,6 +1,7 @@
 
 streaming_subreddits = [
-{'name': 'Soccer Streams', 'url': 'soccerstreams'},
+{'name': 'Soccer Streams', 'url': 'soccerstreams_pl'},
+{'name': 'Other Soccer Streams', 'url': 'soccerstreams_other'},
 {'name': 'MMA Streams', 'url': 'MMAStreams'},
 {'name': 'NFL Streams', 'url': 'NFLStreams'},
 {'name': 'NBA Streams', 'url': 'nbastreams'},
